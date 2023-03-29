e58b8cf23ea56978cdb85ccbe5858a51e4422c69
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-29 22:06:29 +0600

    cosmetic

---
+++ src/components/Consmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Welcome to the Cosmetics</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
