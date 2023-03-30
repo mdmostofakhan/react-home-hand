6d7043df90e604422a91d2fa25d29e88a5712beb
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-30 07:01:13 +0600

    parsent

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { addToBd } from '../Utilities/fakedb';
 import './cosmetic.css'
 
 const Cosmetic = (props) => {
     const{name, price, id} = props.cosmei;
+
+    const AddToCard = (id) =>{
+        addToBd(id);
+    }
+
+
     return (
         <div className='product'>
             
             <h2>Buy this:{name}</h2>
             <p>Only for:${ price}</p>
-            <p>id:{id}</p>
+            <p>It has id:{id}</p>
+            <button onClick={() => AddToCard(id)}>Add To Card</button>
+            {/* <button onClick={() => AddToCard(_id)}>Pashe but</button> */}
+
 
             {/* <h2>Buy this:{props.cosmei.name}</h2>
             <p>Only for:${props.cosmei. price}</p> */}
---
+++ src/components/Utilities/fakedb.js
@@ -0,0 +1,14 @@
+const addToBd = id => {
+  const quantity = localStorage.getItem(id);
+  if(quantity){
+    console.log('already exists')
+    const newQuantity = parseInt(quantity) + 1;
+    localStorage.setItem(id, newQuantity)
+  }
+  else{
+    console.log('new item')
+    localStorage.setItem(id, 1)
+  }
+
+}
+export {addToBd}
