f93d3f86145b3ffd9c7f655889a883f1c4d90a1d
mostofa khan <mdmostofakhan1972@gmail.com>
2023-03-30 13:26:07 +0600

    card

--- src/components/Utilities/fakedb.js
+++ src/components/Utilities/fakedb.js
@@ -1,14 +1,41 @@
 const addToBd = id => {
-  const quantity = localStorage.getItem(id);
+let shopingCart = {}
+// get the shoppingCart
+const storageCard = localStorage.getItem('shoping-cart')
+if(storageCard){
+  shopingCart = JSON.parse(storageCard);
+}
+else{
+  shopingCart = {};
+}
+// add quantity
+  const quantity = shopingCart[id];
   if(quantity){
-    console.log('already exists')
-    const newQuantity = parseInt(quantity) + 1;
-    localStorage.setItem(id, newQuantity)
+    const newQuantity = quantity + 1;
+    shopingCart[id] = newQuantity;
+    // localStorage.setItem(id, newQuantity)
   }
   else{
-    console.log('new item')
-    localStorage.setItem(id, 1)
+      shopingCart[id] = 1;
+    // localStorage.setItem(id, 1)
   }
+  localStorage.setItem('shoping-cart', JSON.stringify(shopingCart))
 
 }
 export {addToBd}
+
+
+// const addToBd = id =>{
+//   const quantity = localStorage.getItem(id)
+//   if(quantity){
+//     console.log('i am quantity')
+//    const newQuantity = parseInt(quantity) + 1;
+//    localStorage.setItem =(id, newQuantity)
+//   }
+//   else{
+//     localStorage.setItem(id, 1)
+//     console.log('new quantity');
+//   }
+// }
+
+// export{addToBd}
